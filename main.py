a7e5b0dca50dc60e9c4e8d16f45edcc9f007cb00
Raffael M. Paranhos <raffaelmp@id.uff.br>
2022-05-08 21:07:24 -0300

    feat: get splits and get merges

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from turtle import left
+from urllib import response
 from fastapi import FastAPI
 from neo4j_connector import Neo4jConnector 
 from fastapi.middleware.cors import CORSMiddleware
 from collections import Counter
 
 app = FastAPI()
-neo4j_conn = Neo4jConnector(uri="neo4j+s://e2731293.databases.neo4j.io", user="neo4j", pwd="fRbjh3yWU83plis5kXRidUAa0Fn7SyyDckTzniUukSE")
+neo4j_conn = Neo4jConnector(uri="neo4j://localhost:7687", user="neo4j", pwd="neo4j")
 
 origins = ["*"]
 
@@ -24,7 +25,7 @@ def hello_world():
 @app.get("/edges/{year}/{block}")
 def get_edges(year: int, block = int):
 
-    query_string = f"MATCH (n:Lot{year} {{block:\"{block}\"}})-[r:INTERSECTION]->(m) RETURN n,r,m"
+    query_string = f"MATCH (n:Lot{year} {{Block:\"{block}\"}})-[r:INTERSECTION]->(m) RETURN n,r,m"
     
     edges = []
     left_edges = []
@@ -49,13 +50,93 @@ def get_edges(year: int, block = int):
         edges.append(edge)
 
  
-    print(left_edges)
     dict_left_edges = dict(Counter(left_edges))
-    print(right_edges)
     dict_right_edges = dict(Counter(right_edges))
 
     for edge in edges:
         edge['left_lot']['exit_edges'] = dict_left_edges[edge['left_lot']['id']]
         edge['right_lot']['incoming_edges'] = dict_right_edges[edge['right_lot']['id']]
-    print(edges) 
     return edges
+
+@app.get("/splits/{year}/{initial_block}/{end_block}")
+def get_splits(year: int, initial_block: int, end_block: int):
+
+    block_list = range(initial_block, end_block)
+    query_string = f"MATCH (n:Lot{year})-[r:INTERSECTION]->(m) WHERE n.Block in {[str(block) for block in block_list]} RETURN n,r,m"
+    
+    edges = []
+    left_edges = []
+    right_edges = []
+    for record in neo4j_conn.query(query_string, db='neo4j'):
+        left_lot = {"id" : record['r'].nodes[0].id}
+        left_edges.append(record['r'].nodes[0].id)
+
+        for item in record['r'].nodes[0].items():
+            left_lot[item[0]] = item[1]
+        
+        intersection = {"id" : record['r'].id}
+        for item in record['r'].items():
+            intersection[item[0]] = item[1]
+        
+        right_lot = {"id" : record['r'].nodes[1].id}
+        right_edges.append(record['r'].nodes[1].id)
+        for item in record['r'].nodes[1].items():
+            right_lot[item[0]] = item[1]
+        
+        edge = {'left_lot':left_lot, 'intersection': intersection, 'right_lot':right_lot}
+        edges.append(edge)
+
+ 
+    dict_left_edges = dict(Counter(left_edges))
+    dict_right_edges = dict(Counter(right_edges))
+
+    response = []
+    for edge in edges:
+        #splits possuem um numero de arestas de saida maior q dois
+        if dict_left_edges[edge['left_lot']['id']] >= 2 :
+            edge['left_lot']['exit_edges'] = dict_left_edges[edge['left_lot']['id']]
+            edge['right_lot']['incoming_edges'] = dict_right_edges[edge['right_lot']['id']]
+            response.append(edge)
+    return response
+
+
+@app.get("/merges/{year}/{initial_block}/{end_block}")
+def get_merges(year: int, initial_block: int, end_block: int):
+
+    block_list = range(initial_block, end_block)
+    query_string = f"MATCH (n:Lot{year})-[r:INTERSECTION]->(m) WHERE n.Block in {[str(block) for block in block_list]} RETURN n,r,m"
+    
+    edges = []
+    left_edges = []
+    right_edges = []
+    for record in neo4j_conn.query(query_string, db='neo4j'):
+        left_lot = {"id" : record['r'].nodes[0].id}
+        left_edges.append(record['r'].nodes[0].id)
+
+        for item in record['r'].nodes[0].items():
+            left_lot[item[0]] = item[1]
+        
+        intersection = {"id" : record['r'].id}
+        for item in record['r'].items():
+            intersection[item[0]] = item[1]
+        
+        right_lot = {"id" : record['r'].nodes[1].id}
+        right_edges.append(record['r'].nodes[1].id)
+        for item in record['r'].nodes[1].items():
+            right_lot[item[0]] = item[1]
+        
+        edge = {'left_lot':left_lot, 'intersection': intersection, 'right_lot':right_lot}
+        edges.append(edge)
+
+ 
+    dict_left_edges = dict(Counter(left_edges))
+    dict_right_edges = dict(Counter(right_edges))
+
+    response = []
+    for edge in edges:
+        #merges possuem um numero de arestas de chegada maior q dois
+        if dict_left_edges[edge['rigth_lot']['id']] >= 2 :
+            edge['left_lot']['exit_edges'] = dict_left_edges[edge['left_lot']['id']]
+            edge['right_lot']['incoming_edges'] = dict_right_edges[edge['right_lot']['id']]
+            response.append(edge)
+    return response    

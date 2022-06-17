2041ac81f78217973d35cd90214bed471c64ac94
Raffael Paranhos <raffaelmp@id.uff.br>
2022-06-16 23:32:41 -0300

    feat: filtro de aresta

--- infrastructure/edge_repository.py
+++ infrastructure/edge_repository.py
@@ -6,6 +6,7 @@ class EdgeRepository:
     
     def __init__(self) -> None:
         self.neo4j_conn = Neo4jHelper(uri="neo4j://localhost:7687", user="neo4j", pwd="neo4j")
+        self.intersect_attribute = ['area','areaA','areaB']
         pass
 
     def get_edges_by_block(self, year: int, block: int):
@@ -17,7 +18,10 @@ class EdgeRepository:
         block_list = range(initial_block, end_block+1)
         query_string = f"MATCH (n:Lot{year})-[r:INTERSECTION]->(m) WHERE n.Block in {[str(block) for block in block_list]}"
         for filter in filter_list:
-            query_string += f" AND n.{filter.attribute} {filter.operand} '{filter.value}'"
+            if filter.attribute in self.intersect_attribute:
+                query_string += f" AND r.{filter.attribute} {filter.operand} {filter.value}"
+            else:    
+                query_string += f" AND n.{filter.attribute} {filter.operand} '{filter.value}'"
         query_string += " RETURN n,r,m"
         print(query_string)
         return self.neo4j_conn.query(query_string, db='neo4j')
--- main.py
+++ main.py
@@ -39,5 +39,3 @@ def get_merges(year: int, initial_block: int, end_block: int):
 def get_rearranges(year: int, initial_block: int, end_block: int):
     return edge_service.get_rearranges(year, initial_block, end_block)
 
-
-#git teste

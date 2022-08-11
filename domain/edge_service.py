2ad14e87b6d62de05aad879c49d460870619bdec
Raffael Paranhos <raffaelmp@id.uff.br>
2022-08-11 15:42:55 -0300

    feat: dicionario de consulta v0

--- domain/edge_service.py
+++ domain/edge_service.py
@@ -123,7 +123,7 @@ class EdgeService:
         left_edges = []
         right_edges = []
         #TODO FILTRO QUEBRADO NO REARRENGE, POIS O FILTRO FAZ COM QUE ELE NAO SE ENCAIXE MAIS NO CRITERIO DE REARRANGE
-        for record in self.edge_repository.get_edges_by_blocklist(year, initial_block, end_block, filter_list):
+        for record in self.edge_repository.get_edges_by_blocklist(year, initial_block, end_block, []): #filter_list):
             left_lot = {"id" : record['r'].nodes[0].id}
             left_edges.append(record['r'].nodes[0].id)
 
@@ -156,10 +156,15 @@ class EdgeService:
             if dict_right_edges[edge['right_lot']['id']] >= 2 and dict_left_edges[edge['left_lot']['id']] >= 2:
                 edge['left_lot']['exit_edges'] = dict_left_edges[edge['left_lot']['id']]
                 edge['right_lot']['incoming_edges'] = dict_right_edges[edge['right_lot']['id']]
-                rearrange_edges = self.insert_edge_ordered(rearrange_edges, edge)
+                if(self.filter_edge(edge,filter_list)):
+                    rearrange_edges = self.insert_edge_ordered(rearrange_edges, edge)
                 rearrange_bbl.append(edge['left_lot']['YearBBL'])
                 rearrange_bbl.append(edge['right_lot']['YearBBL'])
-
+        for item in rearrange_edges:
+            print(item['left_lot']['LotArea'])
+            print(item['right_lot']['LotArea'])
+            print('----')
+        
         #voltando nos splits e merges para capturar toda a participacao do rearranjo
         splits = self.get_splits(year, initial_block, end_block, [])
         for edge in splits:
@@ -188,3 +193,16 @@ class EdgeService:
         if not inserted:
             resp.append(edge_to_insert)
         return resp
+
+    def filter_edge(self, edge, filter_list):
+        for filter in filter_list:
+            result = {
+                '>': lambda attribute, value: edge['left_lot'][attribute] > value or edge['right_lot'][attribute] > value,
+                '<': lambda attribute, value: edge['left_lot'][attribute] < value or edge['right_lot'][attribute] < value,
+                '>=': lambda attribute, value: edge['left_lot'][attribute] >= value or edge['right_lot'][attribute] >= value,
+                '<=': lambda attribute, value: edge['left_lot'][attribute] <= value or edge['right_lot'][attribute] <= value,
+                '=': lambda attribute, value: edge['left_lot'][attribute] == value or edge['right_lot'][attribute] == value,
+                '<>': lambda attribute, value: edge['left_lot'][attribute] != value or edge['right_lot'][attribute] != value,
+                }[filter.operand](filter.attribute, filter.value)
+            if not result: return False
+        return True               

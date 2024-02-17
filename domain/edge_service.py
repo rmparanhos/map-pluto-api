bed085eca0fb9d272d0bed64c15201dabad9f0a3
Raffael Paranhos <raffaelmp@id.uff.br>
2024-02-17 00:23:27 -0300

    fix: retirada de comentarios e debugs

--- domain/edge_service.py
+++ domain/edge_service.py
@@ -74,8 +74,7 @@ class EdgeService:
 
         response = []
         for edge in edges:
-            #splits possuem um numero de arestas de saida maior q dois
-            if dict_left_edges[edge['left_lot']['id']] >= 2: #and dict_right_edges[edge['right_lot']['id']] == 1:
+            if dict_left_edges[edge['left_lot']['id']] >= 2: 
                 edge['left_lot']['exit_edges'] = dict_left_edges[edge['left_lot']['id']]
                 edge['right_lot']['incoming_edges'] = dict_right_edges[edge['right_lot']['id']]
                 response.append(edge)
@@ -111,7 +110,6 @@ class EdgeService:
 
         response = []
         for edge in edges:
-            #merges possuem um numero de arestas de chegada maior q dois
             if dict_right_edges[edge['right_lot']['id']] >= 2: #and dict_left_edges[edge['left_lot']['id']] == 1:
                 edge['left_lot']['exit_edges'] = dict_left_edges[edge['left_lot']['id']]
                 edge['right_lot']['incoming_edges'] = dict_right_edges[edge['right_lot']['id']]
@@ -123,7 +121,6 @@ class EdgeService:
         edges = []
         left_edges = []
         right_edges = []
-        #TODO FILTRO QUEBRADO NO REARRENGE, POIS O FILTRO FAZ COM QUE ELE NAO SE ENCAIXE MAIS NO CRITERIO DE REARRANGE
         for record in self.edge_repository.get_edges_by_blocklist(year, initial_block, end_block, borough, []): #filter_list):
             left_lot = {"id" : record['r'].nodes[0].id}
             left_edges.append(record['r'].nodes[0].id)
@@ -150,31 +147,23 @@ class EdgeService:
         rearrange_edges = []
         rearrange_bbl = []
         for edge in edges:
-            #nxm possuem um numero de arestas de chegada e saida maior q dois
-            #TODO 
-            #FILTRO QUEBRADO NO REARRENGE, POIS O FILTRO FAZ COM QUE ELE NAO SE ENCAIXE MAIS NO CRITERIO DE REARRANGE
-            #E AI NAO ENTRA NESSE IF, EXEMPLO WHERE n.Block in [16] AND (n.LotArea >= 3200.0 OR m.LotArea >= 3200.0) AND (n.Address = 'JOE DIMAGGIO HIGHWAY' OR m.Address = 'JOE DIMAGGIO HIGHWAY')
             if dict_right_edges[edge['right_lot']['id']] >= 2 and dict_left_edges[edge['left_lot']['id']] >= 2:
                 edge['left_lot']['exit_edges'] = dict_left_edges[edge['left_lot']['id']]
                 edge['right_lot']['incoming_edges'] = dict_right_edges[edge['right_lot']['id']]
                 if(self.filter_edge(edge,filter_list)):
-                    #rearrange_edges = self.insert_edge_ordered(rearrange_edges, edge)
                     rearrange_edges.append(edge)
                 
                 rearrange_bbl.append(edge['left_lot']['YearBBL'])
                 rearrange_bbl.append(edge['right_lot']['YearBBL'])
-        #voltando nos splits e merges para capturar toda a participacao do rearranjo
         splits = self.get_splits(year, initial_block, end_block, borough, [])
         for edge in splits:
             if (edge['left_lot']['YearBBL'] in rearrange_bbl or edge['right_lot']['YearBBL'] in rearrange_bbl):
-                #rearrange_edges = self.insert_edge_ordered(rearrange_edges, edge)
                 rearrange_edges.append(edge)
                 rearrange_bbl.append(edge['left_lot']['YearBBL'])
                 rearrange_bbl.append(edge['right_lot']['YearBBL'])
         merges = self.get_merges(year, initial_block, end_block, borough, [])
         for edge in merges:
             if (edge['left_lot']['YearBBL'] in rearrange_bbl or edge['right_lot']['YearBBL'] in rearrange_bbl):
-                #rearrange_edges = self.insert_edge_ordered(rearrange_edges, edge)
                 rearrange_edges.append(edge)
                 rearrange_bbl.append(edge['left_lot']['YearBBL'])
                 rearrange_bbl.append(edge['right_lot']['YearBBL'])
@@ -283,16 +272,13 @@ class EdgeService:
                 edge['left_lot']['exit_edges'] = dict_left_edges[edge['left_lot']['id']]
                 edge['right_lot']['incoming_edges'] = dict_right_edges[edge['right_lot']['id']]
                 if(self.filter_edge(edge,filter_list)):
-                    #rearrange_edges = self.insert_edge_ordered(rearrange_edges, edge)
                     rearrange_edges.append(edge)
                 rearrange_bbl.append(edge['left_lot']['YearBBL'])
                 rearrange_bbl.append(edge['right_lot']['YearBBL'])
         
-        #voltando nos splits e merges para capturar toda a participacao do rearranjo
         splits = self.get_splits(year, initial_block, end_block, borough, [])
         for edge in splits:
             if (edge['left_lot']['YearBBL'] in rearrange_bbl or edge['right_lot']['YearBBL'] in rearrange_bbl):
-                #rearrange_edges = self.insert_edge_ordered(rearrange_edges, edge)
                 rearrange_edges.append(edge)
                 rearrange_bbl.append(edge['left_lot']['YearBBL'])
                 rearrange_bbl.append(edge['right_lot']['YearBBL'])
@@ -300,7 +286,6 @@ class EdgeService:
         merges = self.get_merges(year, initial_block, end_block, borough, [])
         for edge in merges:
             if (edge['left_lot']['YearBBL'] in rearrange_bbl or edge['right_lot']['YearBBL'] in rearrange_bbl):
-                #rearrange_edges = self.insert_edge_ordered(rearrange_edges, edge)
                 rearrange_edges.append(edge)
                 rearrange_bbl.append(edge['left_lot']['YearBBL'])
                 rearrange_bbl.append(edge['right_lot']['YearBBL'])
